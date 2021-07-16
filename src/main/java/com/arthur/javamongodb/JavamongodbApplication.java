22c5ca7336b43dd4dc3265f4383578e3432f592c
Arthur bernardo <arthurutschgg@gmail.com>
2021-07-16 14:41:04 -0300

    first commit

---
+++ src/main/java/com/arthur/javamongodb/JavamongodbApplication.java
@@ -0,0 +1,13 @@
+package com.arthur.javamongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavamongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavamongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthur/javamongodb/JavamongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.javamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavamongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
