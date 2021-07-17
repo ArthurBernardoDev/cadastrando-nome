3b81b1bfd68afa71f99d19a7514a3f9c87794468
Arthur bernardo <arthurutschgg@gmail.com>
2021-07-17 18:19:05 -0300

    mudanças cors

--- src/main/java/com/arthur/javamongodb/resources/UserResource.java
+++ src/main/java/com/arthur/javamongodb/resources/UserResource.java
@@ -14,25 +14,29 @@ import com.arthur.javamongodb.domain.User;
 import com.arthur.javamongodb.services.UserService;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
 	@Autowired
 	private UserService service;
-	
+
+	@CrossOrigin
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+	@CrossOrigin
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 	User obj = service.findById(id);
 	return ResponseEntity.ok().body(new UserDTO(obj));
 		
 	}
+	@CrossOrigin
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
 		User obj = service.fromDTO(objDto);
